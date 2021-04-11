5f85e26ec089ecd73fdb780fc951215f0750faac
Tabata Sutili <ssabrinasutili@gmail.com>
2021-04-11 13:47:11 -0300

    first commit

--- Example/UIKitBecaiOS/ViewController.swift
+++ Example/UIKitBecaiOS/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import UIKitBecaiOS
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        button.setTitle(TitlesButtons.TituloClique, for: .normal)
+        button.setTitle(TitlesButtons.TituloClicado, for: .normal)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ UIKitBecaiOS/Classes/ReplaceMe.swift
@@ -0,0 +1,10 @@
+
+public struct TitlesButtons {
+    public static let TituloClique = "Clique!"
+    public static let TituloClicado = "Clicou!!!"
+}
+
+public class compxxx: UIView {
+    var teste: String = ""
+    
+}
